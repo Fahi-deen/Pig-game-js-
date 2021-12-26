64a82bd552904e83d4d72b6b7ea0a4242f1a61e2
Fahi-deen <80744594+Fahi-deen@users.noreply.github.com>
2021-12-27 00:18:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ btnHold.addEventListener('click', function () {
 		scores[activePlayer] += currentScore;
 		document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
-		if (scores[activePlayer] >= 10) {
+		if (scores[activePlayer] >= 50) {
 			playing = false;
 			diceEl.classList.add(`hidden`);
 			document
